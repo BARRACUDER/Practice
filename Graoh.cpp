86f33a83797b74e8dea0efd9c39c3adcdb93a463
BARRACUDER <121224603+BARRACUDER@users.noreply.github.com>
2025-06-17 15:31:55 +0800

    Add files via upload

---
+++ Graoh.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+
+enum Colors { White, Grey, Black };
+
+class Graph {
+private:
+    std::vector<std::vector<int>> vertices_;
+    std::vector<Colors> colors_;
+
+public:
+    explicit Graph(int n, int m) {
+        vertices_.resize(n);
+        colors_.resize(n, White);
+        for (int i = 0; i < m; i++) {
+            int v1 = 0;
+            int v2 = 0;
+            std::cin >> v1 >> v2;
+            --v1;
+            --v2;
+            vertices_[v1].push_back(v2);
+            vertices_[v2].push_back(v1);
+        }
+    }
+    void DFS(int index) {
+        colors_[index] = Grey;
+        for (int ner : vertices_[index]) {
+            if (colors_[ner] == White) {
+                DFS(ner);
+            }
+        }
+        colors_[index] = Black;
+    }
+
+    void ComponentCount() {
+        int count = 0;
+        for (int i = 0; i < static_cast<int>(vertices_.size()); i++) {
+            if (colors_[i] == White) {
+                DFS(i);
+                count++;
+            }
+        }
+        std::cout << "You have " << count - 1 << " Components\n";
+    }
+};
+
+int main() {
+    int n = 0, m = 0;
+    std::cout << "write your number of vertices: ";
+    std::cin >> n;
+    std::cout << "write your number of edges: ";
+    std::cin >> m;
+    std::cout << "write your edges:\n";
+    Graph graph(n, m);
+    graph.ComponentCount();
+    return 0;
+}
---
+++ PARCTICE.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+
+class fanfic {
+private:
+    std::vector<int> city;
+    int size;
+public:
+    fanfic(int n) : size(n + 2), city(n + 2, 0) {}
+    
+    void modify(int i, int d) {
+        while (i < size) {
+            city[i] += d;
+            i = i | (i + 1);
+        }
+    }
+    
+    void set(int i, int x) {
+        int current = sum(i) - sum(i - 1);
+        int d = x - current;
+        modify(i, d);
+    }
+    
+    int sum(int i) {
+        int res = 0;
+        while (i > 0) {
+            res += city[i];
+            i = (i & (i + 1)) - 1;
+        }
+        return res;
+    }
+    
+    int citysum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+};
+
+int main() {
+    int n, m;
+    std::cout << "write number of streets and number of calls: ";
+    std::cin >> n >> m;
+    std::cout << "write '1 <number> <snow_high>' to change the amount of snow on the street\n";
+    std::cout << "write '2 left_street right_street' to see the amount of snow between those streets\n";
+    fanfic city(n);
+    
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        std::cin >> a >> b >> c;
+        if (a == 1) {
+            city.set(b, c);
+            std::cout << "Snow number succesfuly changed\n";
+        } else if (a == 2) {
+            std::cout << "The sum of snow between streets " << b << " and " << c 
+                      << " is: " << city.citysum(b, c) << "\n";
+        } else {
+            std::cout << "wrong command\n";
+        }
+    }
+    return 0;
+}
